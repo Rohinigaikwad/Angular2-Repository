a58e6e33a8ef74467c8b13451b6d51a3bd9c0066
Rohinigaikwad <rohinigaikwad66@gmail.com>
2019-01-13 19:03:17 +0530

    Component:bootstrap pricing example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,25 @@ import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { InfoActComponent } from './info-act/info-act.component';
+import { BootstrapPricingComponent } from './bootstrap-pricing/bootstrap-pricing.component';
+import { InfoComponent } from './bootstrap-pricing/info.component';
+import { PricingNavComponent } from './bootstrap-pricing/pricing-nav.component';
+import { CardContainerComponent } from './bootstrap-pricing/card-container/card-container.component';
+import { FooterContainerComponent } from './bootstrap-pricing/footer-container/footer-container.component';
+import { PricingCardComponent } from './bootstrap-pricing/card-container/pricing-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
     TitleBarComponent,
-    InfoActComponent
+    InfoActComponent,
+    BootstrapPricingComponent,
+    InfoComponent,
+    PricingNavComponent,
+    CardContainerComponent,
+    FooterContainerComponent,
+    PricingCardComponent
   ],
   imports: [
     BrowserModule
